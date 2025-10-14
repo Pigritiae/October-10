17cad4db6a7477e80835d5ce010e0216889bec2e
Pigritiae <Bernardo.calabria.programacao@gmail.com>
2025-10-14 15:19:21 -0300

    Added Air Traffic Visualizer files (14/10)

---
+++ Folder B 166/OpenStreetMap.js
@@ -0,0 +1,23 @@
+let map;
+function iniciateMap() {
+    map = L.map('map').setView([-22.9, -43.2], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: ' OpenStreetMap contributors'
+}).addTo(map);
+}
+function searchPlanes() {
+    const falseData = [
+        { lat: -22.9, long: -43.2, callsign: 'GOL123', country: 'Brazil'},
+        { lat: -23.5, long: -46.6, callsign: 'LATAM456', country: 'Brazil'},
+        { lat: -19.9, long: -43.9, callsign: 'AZUL789', country: 'Brazil'},
+    ];
+    map.eachLayer(layer => {
+        if (layer instanceof L.Marker) map.removeLayer(layer);
+    });
+    falseData.forEach(plane => {
+        L.marker([plane.lat, plane.long])
+        .addto(map).bindPopup(`<strong>${plane.callsign}</strong><br>Origin: ${plane.country}`);
+    });
+}
+window.onload = iniciateMap
